d0cb2cb20bdb5ed782d1c47d5504f3af57c734e9
FVergaraA <fe.vergaraa@gmail.com>
2019-10-09 10:40:10 -0300

    Mover al inicio, fin y a la mitad el ViewPort

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild( CdkVirtualScrollViewport, { static: false } ) viewport : CdkVirtualScrollViewport;
+
   personas = Array(500).fill(0);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  irFinal() {
+    this.viewport.scrollToIndex( this.personas.length );
+  }
+
+  irInicio() {
+    this.viewport.scrollToIndex( 0 );
+  }
+
+  irMitad() {
+    this.viewport.scrollToIndex( this.personas.length/2 );
   }
 
 }
